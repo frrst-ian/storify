87b7c1773d17b48291fb09bdea7a907b3ee35abc
frrst-ian <frrstxd@proton.me>
2025-08-08 15:38:12 +0800

    chore: remove redundant main function

--- db/prisma.js
+++ db/prisma.js
@@ -1,17 +1,3 @@
-const { PrismaClient } = require("@prisma/client");
-
+const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient;
-
-async function main() {
-
-}
-
-main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+module.exports = prisma;
