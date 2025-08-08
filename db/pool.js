0d0031648e28ff83cb1c30c88db266ccc19fbdb9
frrst-ian <frrstxd@proton.me>
2025-08-08 15:37:17 +0800

    feat: remove redundant js files

--- db/pool.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg");
-
-module.exports = new Pool({
-    connectionString:process.env.DB_URL,
-    ssl: { rejectUnauthorized: false },
-    idleTimeoutMillis: 30000, 
-    connectionTimeoutMillis: 5000,
-})
