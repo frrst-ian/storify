74521d48b1221c1c5f8a9aff6005ba783655ae97
frrst-ian <frrstxd@proton.me>
2025-08-11 10:41:02 +0800

    feat: add edit folder functionality

---
+++ controllers/editFolderController.js
@@ -0,0 +1,46 @@
+const db = require("../db/queries");
+const { validationResult } = require("express-validator");
+
+async function getEditFolderForm(req, res) {
+        const folderId = parseInt(req.params.folderId);
+
+    try {
+        res.render("edit-folder-form", {
+            title: "Edit Folder",
+            folderId
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+}
+
+async function postEditFolderForm(req, res) {
+    const errors = validationResult(req);
+    const folderName = req.body.folderName;
+    const folderId = parseInt(req.params.folderId);
+    
+    if (!errors.isEmpty()) {
+        return res.status(400).render("edit-folder-form", {
+            errorList: errors.array(),
+            title: "Edit Folder",
+            folderName,
+            folderId
+        })
+    }
+
+    try {
+        await db.updateFolderName(folderName, folderId);
+        res.redirect("/");
+    } catch (error) {
+        console.error("Edit Folder Error: ", error);
+        let errorMsg = "Something went wrong. Please try again."
+
+        res.status(500).render("edit-folder-form", {
+            errorList: [{ msg: errorMsg }],
+            title: "Edit Folder"
+        })
+    }
+}
+
+module.exports = { getEditFolderForm, postEditFolderForm };
--- routes/folder.js
+++ routes/folder.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const folderRouter = Router();
 const upload = require("../config/upload");
+const folderValidator = require("../validators/folderValidator");
 const folderController = require("../controllers/folderController");
 const uploadController = require("../controllers/uploadFileController");
+const editFolderController = require("../controllers/editFolderController");
 
 folderRouter.get("/:folderId", folderController.getFolderById);
 folderRouter.get("/:folderId/upload", uploadController.getUploadFileForm);
 folderRouter.post("/:folderId/upload", upload.single('file'), uploadController.postUploadFileForm);
+folderRouter.get("/:folderId/edit", editFolderController.getEditFolderForm);
+folderRouter.post("/:folderId/edit", folderValidator, editFolderController.postEditFolderForm);
 
 module.exports = folderRouter;
