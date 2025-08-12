67e77b98207a07fb8c3aafe5d5347d2abc81c7a3
frrst-ian <frrstxd@proton.me>
2025-08-12 10:35:37 +0800

    feat: add delete functionality for folders

--- controllers/deleteFolderController.js
+++ controllers/deleteFolderController.js
@@ -1,9 +1,8 @@
 const db = require("../db/queries");
 
-async function getFolderById(req, res, next) {
+async function postDeleteFolderForm(req, res) {
     const folderId = parseInt(req.params.folderId);
     const userId = req.user.id;
-
 
     try {
         const folder = await db.getFolderById(folderId);
@@ -11,26 +10,18 @@ async function getFolderById(req, res, next) {
             req.flash("error", "Folder doesn't exist");
             return res.status(404).redirect("/");
         }
-
         if (folder.userId === userId) {
-            const filesInFolder = await db.getFilesInFolder(userId, folderId)
-            res.render("folder-files", {
-                folder,
-                filesInFolder,
-                title: "Folder | Storify",
-                user: req.user,
-                inFolder :true
-            })
+            await db.deleteFolder(folderId);
+            req.flash("success", "Folder deleted");
+            res.redirect("/");
         } else {
             req.flash("error", "Permission denied");
             return res.status(401).redirect("/");
         }
-
-
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
     }
 }
 
-module.exports = { getFolderById }
+module.exports = { postDeleteFolderForm }
--- routes/folder.js
+++ routes/folder.js
@@ -5,11 +5,13 @@ const folderValidator = require("../validators/folderValidator");
 const folderController = require("../controllers/folderController");
 const uploadController = require("../controllers/uploadFileController");
 const editFolderController = require("../controllers/editFolderController");
+const deleteFolderController = require("../controllers/deleteFolderController")
 
 folderRouter.get("/:folderId", folderController.getFolderById);
 folderRouter.get("/:folderId/upload", uploadController.getUploadFileForm);
 folderRouter.post("/:folderId/upload", upload.single('file'), uploadController.postUploadFileForm);
 folderRouter.get("/:folderId/edit", editFolderController.getEditFolderForm);
 folderRouter.post("/:folderId/edit", folderValidator, editFolderController.postEditFolderForm);
+folderRouter.post("/:folderId/delete", deleteFolderController.postDeleteFolderForm);
 
 module.exports = folderRouter;
