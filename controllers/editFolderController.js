8a6202e20b2311587e51a678991661bb3b2eb6d4
frrst-ian <frrstxd@proton.me>
2025-08-12 11:02:31 +0800

    feat: prefill folder name when editing

--- controllers/editFolderController.js
+++ controllers/editFolderController.js
@@ -2,11 +2,14 @@ const db = require("../db/queries");
 const { validationResult } = require("express-validator");
 
 async function getEditFolderForm(req, res) {
-        const folderId = parseInt(req.params.folderId);
+    const folderId = parseInt(req.params.folderId);
+
 
     try {
+        const folder = await db.getFolderById(folderId);
         res.render("edit-folder-form", {
             title: "Edit Folder",
+            folderName: folder.name,
             folderId
         })
     } catch (error) {
@@ -19,7 +22,7 @@ async function postEditFolderForm(req, res) {
     const errors = validationResult(req);
     const folderName = req.body.folderName;
     const folderId = parseInt(req.params.folderId);
-    
+
     if (!errors.isEmpty()) {
         return res.status(400).render("edit-folder-form", {
             errorList: errors.array(),
